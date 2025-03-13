39e48aca26cac4a0d88527c97e5383e24661ce68
yixxuz18 <yisuskroom@gmail.com>
2025-03-13 16:15:14 -0600

    HOLA

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    copyPublicDir: true
-  },
-  publicDir: 'public',
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://24.199.119.80',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
